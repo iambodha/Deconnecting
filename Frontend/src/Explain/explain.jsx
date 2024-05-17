fb13249d197c7be86cb3d4178eaa1e4e9e195f84
Kerem Semiz <kerox350@gmail.com>
2024-05-17 19:16:00 +0300

    Add Explain area for site, further refinements will be made!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Frontend/src/Explain/explain.jsx
+++ Frontend/src/Explain/explain.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../Hero/hero.css';
 import './explain.css';
-import textureImage from '../assets/texture.png'; // bg texture
+import icon1 from '../assets/icon1.png'
+import icon2 from '../assets/icon1.png'
+import icon3 from '../assets/icon1.png'
+import explainImg from '../assets/explain1.jpg'
 
 function Explain() {
-
     return (
         <div className='explainContainer'>
-            <h1 className='archivo'>Test</h1>
+            <h1 className='headertext archivo'>What's Deconnecting?</h1>
+            <div className="explainMain">
+                <div className='explainLeft'>
+                    <h2 className='bigHeader'>Deconnecting is...</h2>
+                    <div className='smallPoint'>
+                        <img src={icon1} alt='Icon 1' className='icon'/>
+                        <p>Small Point 1</p>
+                    </div>
+                    <div className='smallPoint'>
+                        <img src={icon2} alt='Icon 2' className='icon'/>
+                        <p>Small Point 2</p>
+                    </div>
+                    <div className='smallPoint'>
+                        <img src={icon3} alt='Icon 3' className='icon'/>
+                        <p>Small Point 3</p>
+                    </div>
+                </div>
+                <div className='explainRight'>
+                    <img src={explainImg} alt='Explanation' className='explainImage'/>
+                </div>
+            </div>
         </div>
-
     );
 }
 
-export default Explain
+export default Explain;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Hero />
-    <Explore />
     <Explain />
+    <Explore />
   </React.StrictMode>,
 )
